b49a2a143e6de950aeae4959bf1ccc18a601f443
alejandro012004 <150953789+alejandro012004@users.noreply.github.com>
2026-02-16 21:33:55 +0100

    Preparando Firebase para despliegue en la nube

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -1,12 +1,30 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccountKey.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../../serviceAccountKey.json');
 
-// Conexión con las credenciales de Firebase
+// // Conexión con las credenciales de Firebase
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// const db = admin.firestore();
+
+// // Exportamos la base de datos para usarla en otros archivos
+// module.exports = db;
+
+const admin = require('firebase-admin');
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    serviceAccount = require('../../serviceAccountKey.json');
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
 
-// Exportamos la base de datos para usarla en otros archivos
 module.exports = db;
